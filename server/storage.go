bd6487ba2079bacc26841a246d7d3eff5619be06
Petro_Osypenko <v-posypenko@expediagroup.com>
2021-02-24 17:59:38 +0200

    PART 3. for review. not working :(

--- server/storage.go
+++ server/storage.go
@@ -11,8 +11,6 @@ type User struct {
 	Age     int    `json:"age"`
 }
 
-//var storage []User
-
 type Storage interface {
 	GetUser(id string) (*User, error)
 	CreateUser(*User) error // Make sure ID is added to the struct OR return ID from this method
@@ -54,45 +52,8 @@ func (us *UserStorage) DeleteUser(id string) error {
 	return nil
 }
 
-// func (s *Server) CreateUser(c echo.Context) error {
-// 	user := User{}
-// 	user.ID = genUUID()
-// 	err := c.Bind(&user)
-// 	if err != nil {
-// 		return echo.NewHTTPError(http.StatusUnprocessableEntity)
-// 	}
-// 	storage = append(storage, user)
-// 	return c.JSON(http.StatusCreated, user)
-// }
-
-// func (s *Server) GetUser(c echo.Context) error {
-// 	id := c.Param("id")
-// 	for _, user := range storage {
-// 		if user.ID == id {
-// 			return c.JSON(http.StatusOK, user)
-// 		}
-// 	}
-// 	return c.JSON(http.StatusBadRequest, nil)
-// }
-
-// func (s *Server) DeleteUser(c echo.Context) error {
-// 	id := c.Param("id")
-// 	for item := range storage {
-// 		if storage[item].ID == id {
-// 			storage = append(storage[:item], storage[item+1:]...)
-// 			return c.JSON(http.StatusOK, "User was deleted")
-// 		}
-// 	}
-// 	return c.JSON(http.StatusBadRequest, nil)
-// }
-
 func NewInMemoryStorage() Storage {
 	// TODO: IMPLEMENT PART A
-	// storage = append(storage, User{genUUID(), "Mike", "Turtle", 18})
-	// storage = append(storage, User{genUUID(), "Leo", "Turtle", 18})
-	// storage = append(storage, User{genUUID(), "Splinter", "Rat", 300})
-	// storage = append(storage, User{genUUID(), "Shredder", "Human", 32})
-
 	return &UserStorage{
 		storage: []User{
 			{genUUID(), "Shredder", "Human", 32},
